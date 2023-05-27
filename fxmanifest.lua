2dc36a681acd52e5acdccba61bc90dd33a070aff
LiOr9 <134813051+1LiOrE9@users.noreply.github.com>
2023-05-28 00:57:55 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+ 
+description 'Example Manifest'
+version '1.0.0'
+ 
+lua54 'yes'
+
+client_scripts {
+    'client.lua'
+}
+shared_scripts { 
+    'config.lua'
+}
