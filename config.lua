79de90dd27210aee6fdd43c66fd6a4e1415edc66
LiOr9 <134813051+1LiOrE9@users.noreply.github.com>
2023-05-28 00:58:06 +0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,14 @@
+Config = {}
+-- Config Options
+Config.PoliceJobs = {'police'} -- {'bcso', 'police'} 
+Config.Command = 'pfix' -- Your Command Name
+Config.Coords = 439.83392, -1016.893, 28.726852 -- Standard Coords..
+Config.ProgressbarTime = 5000 -- In MS (1Second = 10000ms) (5Seconds = 50000ms)
+Config.useWhileDead = false
+Config.canCancel = true
+
+-- Locales
+Config.NoVehicle = 'You Are Not In Any Vehicle!'
+Config.NotPolice = 'You Are Not Police Officer!'
+Config.Location = 'You Cant Do That Here!'
+Config.Progressbar = 'Fixing You Vehicle...'
